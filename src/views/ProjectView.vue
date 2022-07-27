b569f13b82c6a6c1e32130065633549167f0fb4a
oswvld <daniel.demonteverde@gmail.com>
2022-07-27 13:59:45 +0200

    params

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,3 +1,12 @@
 <template>
-  Project view
+  Project view {{$route.params.id}}
 </template>
+
+<script>
+export default {
+  mounted() {
+    // get id parameter
+    console.log(this.$route.params.id)
+  }
+}
+</script>
