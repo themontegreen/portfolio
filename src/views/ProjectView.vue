ec00c07bb908284aedc247eb4bf5a7ba2c240b6b
oswvld <daniel.demonteverde@gmail.com>
2022-07-27 16:04:35 +0200

    added gsap

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,12 +1,21 @@
 <template>
-  Project view {{$route.params.id}}
+  <div class="project-top-bar">
+    Project view {{$route.params.id}}
+    <div class="name-project">{{title}}</div>
+    <router-link
+      class="close-btn"
+      :to="{name: 'home'}"
+    >
+      <img style="width: 18px;" src="/img/x-button.png" alt="">
+    </router-link>
+
+  </div>
 </template>
 
 <script>
+import gsap from "gsap";
 export default {
   mounted() {
-    // get id parameter
-    console.log(this.$route.params.id)
   }
 }
 </script>
