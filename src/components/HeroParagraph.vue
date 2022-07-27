b695008245615e3e19af316354c0c5cb4175448d
oswvld <daniel.demonteverde@gmail.com>
2022-07-27 15:25:22 +0200

    added sass and fonts

--- src/components/HeroParagraph.vue
+++ src/components/HeroParagraph.vue
@@ -1,4 +1,24 @@
 <template>
-  <div class="hero-paragraph" v-text="'prova'">
+  <div class="hero-paragraph">
+    Milan based designer+developer focused in the design and development process of visual design systems, digital user experiences and interactive data-driven content. 
   </div>
 </template>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/colors.scss';
+@import '@/assets/scss/fonts.scss';
+.hero-paragraph{
+  @include paragraph-big;
+  position: relative;
+  width: 100vw;
+  padding: 16px;
+  border-bottom: 1px solid $true-black;
+}
+
+@media screen and (max-width: 767px) {
+  .hero-paragraph{
+    @include paragraph-small;
+    width: auto;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import it from "./assets/content_IT.json";
-import en from "./assets/content_EN.json";
+import it from "./assets/content/content_IT.json";
+import en from "./assets/content/content_EN.json";
 
 const app = createApp(App)
 app.use(router)
