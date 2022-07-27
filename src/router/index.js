c90a4929f13062d9fabfd40ccd9b8e89218ba5e8
oswvld <daniel.demonteverde@gmail.com>
2022-07-27 11:56:30 +0200

    custom routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import Homepage from '../views/Homepage.vue'
+import HomepageView from '../views/HomepageView.vue'
+import ProjectView from '../views/ProjectView.vue'
+import NotFound from '../views/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,16 +9,18 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: Homepage
+      component: HomepageView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/project-:id',
+      name: 'project',
+      component: ProjectView
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: NotFound
+    },
   ]
 })
 
