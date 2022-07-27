1616f81d13147d887dd9b62595f76296e57aa1e3
oswvld <daniel.demonteverde@gmail.com>
2022-07-27 14:07:50 +0200

    added content

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import it from "./assets/content_IT.json";
+import en from "./assets/content_EN.json";
 
 const app = createApp(App)
 
