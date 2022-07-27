79c73b8803612582f08f63cc253d5bfadcfa046f
oswvld <daniel.demonteverde@gmail.com>
2022-07-27 11:38:50 +0200

    setup homepage view

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,8 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
+  <RouterView />
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -16,8 +17,7 @@ import HelloWorld from './components/HelloWorld.vue'
       </nav>
     </div>
   </header>
-
-  <RouterView />
+  <RouterView /> -->
 </template>
 
 <style scoped>
