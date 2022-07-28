d86a9787d9ded184d3c25d304da6523aac076fe1
oswvld <daniel.demonteverde@gmail.com>
2022-07-28 21:02:24 +0200

    added styles

--- src/components/ProjectShowcase.vue
+++ src/components/ProjectShowcase.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="project-showcase">
+    <div class="media">
+      
+    </div>
   </div>
 </template>
 
@@ -12,7 +15,7 @@ export default {
     }
   },
   mounted(){
-    
+    console.log(this.media)
   },
   methods:{
   }
@@ -20,4 +23,39 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import './src/assets/scss/colors.scss';
+@import './src/assets/scss/fonts.scss';
+.project-showcase{
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  .media{
+    position: relative;
+    width: 100%;
+  }
+}
+
+/* Mobile */
+@media screen and (max-width: 1200px){
+}
+
+/* Mobile (Landscape) */
+@media screen and (max-width: 1200px) and (orientation: landscape){
+  .project-showcase{
+    .media{
+    }
+  }
+}
+
+/* 1920 x 1080 */
+@media screen and (min-width: 1920px) and (orientation: landscape){
+  .project-showcase{
+    .media{
+    }
+  }
+}
 </style>
--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="project-top-bar">
+  <div class="project-page">
     <ProjectTopBar :title="title"/>
     <div class="project-info">
       <div class="container-carousel">
@@ -52,3 +52,117 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import './src/assets/scss/colors.scss';
+@import './src/assets/scss/fonts.scss';
+.project-page{
+  max-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  .project-info{
+    position: relative;
+    width: 100%;
+    height: calc(100vh - 80px);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    margin-top: 40px;
+    .container-carousel{
+      position: relative;
+      height: 50%;
+      border-bottom: 1px solid $true-black;
+      padding: 8px;
+    }
+    .project-showcase{
+      position: relative;
+    }
+    .project-content{
+      position: relative;
+      width: 100%;
+      height: 50%;
+      .info-header{
+        display: flex;
+        height: 40px;
+        flex-direction: row;
+        .info{
+          width: 100%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          border-right: 1px solid $true-black;
+          @include title-small;
+          &:last-child{
+            border-right: none;
+          }
+        }
+      }
+      .container-paragraph{
+        position: relative;
+        height: calc(100% - 56px);
+        border-top: 1px solid $true-black;
+        padding: 8px 16px;
+        .paragraph{
+          position: relative;
+          height: 100%;
+          overflow: scroll;
+          @include paragraph;
+        }
+      }
+    }
+  }
+}
+
+/* Mobile (Landscape) */
+@media screen and (max-width: 1200px) and (orientation: landscape){
+  .project-page{
+    .project-info{
+      flex-direction: row;
+      height: calc(100vh - 80px);
+      .container-carousel{
+        width: 45%;
+        height: calc(100% - 16px);
+      }
+      .project-content{
+        width: 55%;
+        height: 100%;
+        border-left: 1px solid $true-black;
+      }
+    }
+  }
+}
+
+/* Tablet Landscape */
+@media screen and (min-width: 1200px) and (orientation: landscape){
+  .project-page{
+    .project-info{
+      flex-direction: row-reverse;
+      height: calc(100vh - 80px);
+      .container-carousel{
+        width: 60%;
+        height: calc(100% - 16px);
+        border-left: 1px solid $true-black;
+      }
+      .project-content{
+        width: 40%;
+        height: 100%;
+        border-left: 1px solid $true-black;
+      }
+    }
+  }
+}
+
+/* 1920 x 1080 */
+@media screen and (min-width: 1366px) and (orientation: landscape){
+  .project-page{
+    .project-info{
+      .container-carousel{
+        width: 65%;
+      }
+      .project-content{
+        width: 35%;
+      }
+    }
+  }
+}
+</style>
