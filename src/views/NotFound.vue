73d5cf3e8088c1d46e0fc8969cf65a32b87be7cd
oswvld <daniel.demonteverde@gmail.com>
2022-08-03 01:08:32 +0200

    edited 404 page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,3 +1,27 @@
 <template>
-  404
+  <div class="page-not-found">
+    <div class="error">404</div>
+    <div class="description">page not found :(</div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+@import './src/assets/scss/colors.scss';
+@import './src/assets/scss/fonts.scss';
+.page-not-found{
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  .error{
+    @include title-small;
+  }
+  .description{
+    @include title-small;
+    font-weight: 300;
+  }
+}
+</style>
