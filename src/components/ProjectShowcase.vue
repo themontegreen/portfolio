037f7a0ba2ac945dcf426cd30b8f1b3a338c1b3b
oswvld <daniel.demonteverde@gmail.com>
2022-08-04 13:45:08 +0200

    added flickity

--- src/components/ProjectShowcase.vue
+++ src/components/ProjectShowcase.vue
@@ -1,27 +1,39 @@
 <template>
   <div class="project-showcase">
-    <div class="media">
-      <img :src="'./img/projects/'+id+'/'+media[0]" alt="">
+    <div class="media" data-flickity='{ "cellAlign": "left", "contain": true }'>
+      <img
+        v-for="(media,m) in media"
+        :key="m"
+        class="carousel-cell"
+        :src="'./img//projects/'+id+'/'+media"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import Flickity from "flickity"
 export default {
   name: "ProjectShowcase",
   props: ["id","media"],
   data(){
     return {
+      flkty: null
     }
   },
   mounted(){
+    // this.flkty = new Flickity(this.$el.querySelector('.project-showcase .media'), {
+    //   // options
+    //   cellAlign: 'left',
+    //   contain: true
+    // });
   },
   methods:{
   }
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 @import './src/assets/scss/colors.scss';
 @import './src/assets/scss/fonts.scss';
 .project-showcase{
@@ -30,9 +42,9 @@ export default {
   height: 100%;
   overflow: hidden;
   .media{
-    img{
-      position: absolute;
-      height: 100%;
+    height: 100%;
+    .carousel-cell{
+      height: 200px;
     }
   }
 }
